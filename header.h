44f10c7341ed6c8f75a27bcd8b1c0b66683b3bc8
duyanhhn2004 <137622061+duyanhhn2004@users.noreply.github.com>
2024-08-17 15:31:42 +0700

    Update header.h

--- header.h
+++ header.h
@@ -12,3 +12,4 @@ void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 void waitUntilKeyPressed();
 
 SDL_Texture* loadTexture(string path, SDL_Renderer* renderer);
+bool CrashInto(int x, int y, int w, int h, int ox, int oy, int ow, int oh);
